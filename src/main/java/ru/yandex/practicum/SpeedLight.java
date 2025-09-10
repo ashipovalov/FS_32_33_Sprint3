a79eb4dd9de363308b0a63b264f15d7c656460ab
ashipovalov <ashipovalov@gmail.com>
2025-09-10 19:36:24 +0300

    Реализован метод showLight

---
+++ src/main/java/ru/yandex/practicum/SpeedLight.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum;
+
+public class SpeedLight {
+    public String showLight(int currentSpeed) {
+        if (currentSpeed < 60) {
+            return "green";
+        } else if (currentSpeed < 80) {
+            return "yellow";
+        } else {
+            return "red";
+        }
+    }
+
+}
